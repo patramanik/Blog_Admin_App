fa04dca556a6bc76be327a2a28d2a0399c1e335d
patramanik <patramanik409@gmail.com>
2023-09-05 00:58:11 +0530

    fix ckeditor

--- app/Http/Controllers/Admin/BlogPostController.php
+++ app/Http/Controllers/Admin/BlogPostController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\File;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Admin\PostFormRequest;
+
 
 class BlogPostController extends Controller
 {
@@ -53,73 +53,19 @@ class BlogPostController extends Controller
     }
 
 
-    // public function submit(Request $request) {
-
-    //     $data=$request->validated([
-    //         'category_id'=>[
-    //             'required',
-    //         ],
-    //         'post_name'=>[
-    //             'required',
-    //             'string',
-    //             'max:200'
-    //         ],
-    //         'mata_title'=>[
-    //             'required',
-    //             'string',
-    //             'max:200'
-    //         ],
-    //         'image'=>[
-    //             'nullable',
-    //             'mimes:jpeg,jpg,png'
-    //         ],
-    //         'post_content'=>[
-    //             'required',
-    //             'string'
-    //         ]
-    //     ]);
-    //     $post = new Post;
-    //     $post->category_id=$data['catagory_id'];
-    //     $post->post_name = $data['post_name'];
-    //     $post->mata_title = $data['mataTile'];
-
-    //     if ($request->hasFile('image')) // Corrected the method name to hasFile
-    //     {
-    //         $file = $request->file('image'); // Corrected the variable name from $data to $request
-    //         $filename = time() . '.' . $file->getClientOriginalExtension(); // Used getClientOriginalExtension() method
-    //         $file->move('uploads/post/', $filename);
-    //         $post->image = $filename;
-    //     }
-
-    //     $post->image = $data['image'];
-    //     $post->post_content = $data['Post_Content'];
-    //     $post->created_by = Auth::user()->id;
-    //     $post->save();
-
-    //     return redirect('admin/posts')->with('message','Post added Successfully') ;
-    // }
-
     // public function store(Request $request){
-    //     $request->validate([
-    //         'category_id' => 'nullable',
-    //         'post_name' => 'nullable|string|max:200',
-    //         'meta_title' => 'nullable|string|max:200', // Fixed the field name
-    //         'image' => 'nullable|mimes:jpeg,jpg,png',
-    //         'post_content' => 'nullable|string',
-    //     ]);
     //     dd($request->all());
     //     return view('admin.blogPost.posts');
     // }
 
-    public function upload(Request $request){
-        if($request->hasFile('upload')){
-            $file = $request->file('upload'); // Corrected the variable name from $data to $request
-            $filename = time() . '.' . $file->getClientOriginalExtension(); // Used getClientOriginalExtension() method
-            $file->move('uploads/post/upload/', $filename);
-            $url = asset('uploads/post/upload/' . $fileName);
+    public function upload(Request $request) {
+        if ($request->hasFile('upload')) {
+            $file = $request->file('upload'); // Get the uploaded file
+            $filename = time() . '.' . $file->getClientOriginalExtension(); // Generate a unique filename
+            $file->move('uploads/post/upload/', $filename); // Move the file to the desired directory
+            $url = asset('uploads/post/upload/' . $filename); // Generate the URL for the uploaded file
 
-         return response()->json(['fileName' => $fileName,'uploded'=>1,'url' =>$url ]);
-
+            return response()->json(['fileName' => $filename, 'uploaded' => 1, 'url' => $url]);
         }
     }
 }
