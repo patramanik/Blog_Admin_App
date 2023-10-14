5d0243e74eee3f840e482fe7cc968ee642519a60
Manik patra <72035800+patramanik@users.noreply.github.com>
2023-10-14 17:32:32 +0530

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,7 @@
   <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet" />
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   <link
     href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@500&family=Lora:ital,wght@1,600&family=Raleway:wght@600&family=Roboto+Mono&family=Roboto+Slab&display=swap"
     rel="stylesheet">
